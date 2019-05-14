152f5e00a875a95240715ad98c4e11f2be3d82db
ecrapps <dan.zerbib@deutschebahn.com>
2019-05-14 11:41:04 +0200

    Removed unnecessary files

--- public/settings/settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-	
-	$config['displayErrorDetails'] = true;
-	$config['addContentLengthHeader'] = false;
-
-	$config['db']['host']   = "localhost";
-	$config['db']['user']   = "devreq";
-	$config['db']['pass']   = "9Fp8otcmDWTxIYZpYLpU";
-	$config['db']['dbname'] = "dev_requests";
